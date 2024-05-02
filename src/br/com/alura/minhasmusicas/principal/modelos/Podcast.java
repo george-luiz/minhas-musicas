3f686fbdfc29cfcfffb5e9c8d66998bcbd26bfa0
george-luiz <geluiz275@gmail.com>
2024-05-02 16:05:00 -0300

    Adicionado Polimorfismo na classe: MinhasPreferencias.

--- src/br/com/alura/minhasmusicas/principal/modelos/Podcast.java
+++ src/br/com/alura/minhasmusicas/principal/modelos/Podcast.java
@@ -1,2 +1,32 @@
-package br.com.alura.minhasmusicas.principal.modelos;public class Podcast {
+package br.com.alura.minhasmusicas.principal.modelos;
+
+public class Podcast extends Audio {
+    private String Apresentador;
+    private String descricao;
+
+    public String getApresentador() {
+        return Apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        Apresentador = apresentador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if(this.getTotalCurtidas() > 500) {
+            return 10;
+        } else {
+            return 8;
+        }
+
+    }
 }
--- src/br/com/alura/minhasmusicas/principal/principal/Principal.java
+++ src/br/com/alura/minhasmusicas/principal/principal/Principal.java
@@ -1,5 +1,37 @@
-public class Main {
+package br.com.alura.minhasmusicas.principal.principal;
+
+import br.com.alura.minhasmusicas.principal.modelos.MinhasPreferidas;
+import br.com.alura.minhasmusicas.principal.modelos.Musica;
+import br.com.alura.minhasmusicas.principal.modelos.Podcast;
+
+public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduzir();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduzir();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
     }
 }
